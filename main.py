089d4ef9a6422dad70e2b08116aae9813396c063
Leon <l.fretter@addon-solution.de>
2019-10-29 17:00:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from subprocess import Popen, PIPE, call
+import sys
+
+if len(sys.argv) != 2:
+    print('usage: cmd brightess')
+    exit()
+
+brightness = sys.argv[1]
+
+if brightness < .2:
+    print('Dude, dont you want to be able to read something?')
+    exit()
+
+cmd = Popen(['xrandr', '-q'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+
+
+
+stdout, stderr = cmd.communicate()
+stdout_str = stdout.decode('utf-8')
+
+lines = stdout_str.splitlines()
+
+
+for line in lines:
+    if 'connected' in line:
+        words = line.split()
+        connection = words[0]
+        call(['xrandr', '--output', connection, '--brightness', brightness])
